c647deef0fb9e728cc29e09f37c3aabc3953def5
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-10-21 22:26:06 +0530

    Remove unuse files

--- src/sub_components/claint_m_form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function claint_m_form() {
-  return (
-    <div className='h-100 w-100 bg-blue-600'>claint_m_form</div>
-  )
-}
-
-export default claint_m_form
--- src/sub_components/sales_m_form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function sales_m_form() {
-  return (
-    <div>sales_m_form</div>
-  )
-}
-
-export default sales_m_form
