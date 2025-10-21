2dc3cc6274d8b03dd13e543717c492b3247a7acd
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-10-21 22:28:28 +0530

    create loding component files

---
+++ src/sub_components/loding.jsx
@@ -0,0 +1,12 @@
+import {React} from "react";
+
+
+function Loding() {
+  return (
+    <div className="grid items-center justify-center h-screen">
+      <div className="w-16 h-16 border-8 border-t-blue-500 border-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loding;
