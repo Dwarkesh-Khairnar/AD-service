c8739c83971e88a5406b0ad1b4c4de1479b43117
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-10-27 22:46:38 +0530

    Desing meeting home

--- src/sub_components/meeting_home.jsx
+++ src/sub_components/meeting_home.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import Router components
-import Meet_form from "../assets/Meet_form.svg"
-import Meet_join from "../assets/Meet_join.svg"
-
+import Meet_form from "../assets/Meet_form.svg";
+import Meet_join from "../assets/Meet_join.svg";
+import man_reisent from "../assets/man_with_responsive.svg"
 function meeting_home() {
   return (
-    <div className="h-screen w-screen grid justify-center">
-
-   <ul className="gap-13 flex mt-5 h-10">
-          <li
-            className="w-10"
-            // onClick={create_new_meet}
-            >
-            <Link to="/client"><img className="ml-2 h-15 w-12" src={Meet_form} alt="" /></Link>
-            Shedule Meeting
-          </li>
-          <li className=" w-10 ">
-            <Link to="/meetjoin"><img className="ml-2 h-15 w-12" src={Meet_join} alt="" srcset="" /></Link>
-          Join Meeting
-          </li>
-        </ul>
-            </div>
+    <div className="">
+      <div className=" h-45 grid items-center justify-center">
+<h1>Welcome to Our Meeting Hub</h1>
+        <p>Bridging Connections for Success</p>
+      </div>
+      <div className="h-35 mt-4 flex justify-center gap-14">
+        <div className="">
+          <ul className=" flex mt-5 h-10">
+            <li className="w-10">
+              <Link to="/client">
+                <img className="ml-2 h-15 w-12" src={Meet_form} alt="" />
+                <span className="text-sm text-center grid">
+                  Requist Meeting
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="bg-gray-500 w-0.5"></div>
+        <div>
+          <ul className=" flex mt-5 h-10">
+            <li className=" w-10 ">
+              <Link to="/meetjoin">
+                <img
+                  className="ml-1.5 h-15 w-12"
+                  src={Meet_join}
+                  alt=""
+                  srcset=""
+                />
+              </Link>
+              <span className="text-sm text-center grid">Join Meeting</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center mt-20">
+        <img src={man_reisent} className="h-80" />
+      </div>
+    </div>
   );
 }
 
