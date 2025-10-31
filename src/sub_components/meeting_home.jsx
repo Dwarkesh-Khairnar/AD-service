8f9479ffdf6dee5b1d9d56c68d488548d1e0acaf
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-10-31 14:24:39 +0530

    Meeting home responsive on mobile and pc

--- src/sub_components/meeting_home.jsx
+++ src/sub_components/meeting_home.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import Router components
 import Meet_form from "../assets/Meet_form.svg";
 import Meet_join from "../assets/Meet_join.svg";
-import man_reisent from "../assets/man_with_responsive.svg";
+import man_reisent from "../assets/desiner_on_laptop_back.svg";
 function meeting_home() {
   return (
     <div className="">
+      <p className="font-mono ml-2 mt-0.5">Guide</p>
       <div className=" h-45 grid items-center justify-center">
         <h1 className=" font-semibold text-[300%]">Welcome to Our Meeting Hub</h1>
-        <p className=" font-sans">Bridging Connections for Success</p>
+        <p className="font-sans md:ml-[33%]">Bridging Connections for Success</p>
       </div>
+      <div className="mt-10 md:flex md:justify-center md:ml-10 md:w-screen md:items-center-safe">
       <div className="h-35 md:mt-4 flex justify-center mt-16 gap-14">
         <div className="">
           <ul className=" flex mt-5 h-10">
@@ -40,11 +42,11 @@ function meeting_home() {
           </ul>
         </div>
       </div>
-      <div className="flex justify-center md:mt-20">
-        <img src={man_reisent} className="h-80" />
-      </div>
+      <div className="mt-3 md:h-80 md:ml-30">
+        <img src={man_reisent} className="h-80 md:h-70 " />
+      </div></div>
       <div className="w-screen flex justify-center mt-3">
-      <span className="text-red-600">This feacher is only for cleint and Company Representative discution </span>
+      <span className="text-red-600"> <span className="text-amber-600">Warn: </span>This feacher is only for cleint and Company Representative discution </span>
 
       </div>
     </div>
