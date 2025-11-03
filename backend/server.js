a2c02d9f8c598b4964a207c27db1b5e8043af7ec
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-11-03 22:19:22 +0530

    Add random api key generate function up-1

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 // backend/server.js
 import express from "express";
-// import cors from "cors";
+import cors from 'cors'
 import axios from "axios";
-const PORT =3000;
+const PORT = 3000;
 
 const app = express();
-// app.use(cors());           // allow frontend to call backend
-// app.use(express.json());   // parse JSON bodies
+app.use(cors());           // allow frontend to call backend
+app.use(express.json());   // parse JSON bodies
 
 // Test API route
 app.get("/", (req, res) => {
-  res.send(`<div style="height: 100%; width: 100%; ">
+    res.send(`<div style="height: 100%; width: 100%; ">
 
 <div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
     <div style="">
@@ -21,6 +21,26 @@ app.get("/", (req, res) => {
 
 </div>`);
 });
+
+app.get('/create-key', async (req, res) => {
+    const characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+        ];
+    let result = '';
+
+   for (let index = 0; index < 23; index++) {
+    const randum_num=Math.floor(Math.random()*characters.length);
+    console.log(randum_num);
+    
+    result +=characters[randum_num]
+   }
+   return res.json({result})
+});
+
 const apiKeys = {
     'A1': { userId: 1, name: 'User One' },
     'user2_key': { userId: 2, name: 'User Two' },
@@ -28,7 +48,7 @@ const apiKeys = {
 };
 
 app.get('/fetch-date', async (req, res) => {
-    const { url, apiKey,limit } = req.query;
+    const { url, apiKey, limit } = req.query;
     // Replace with your actual API key for validation
 
     // Validate API Key
@@ -43,13 +63,13 @@ app.get('/fetch-date', async (req, res) => {
 
     try {
         const response = await axios.get(url);
-       const data = response.data;
-       const date = new Date().toString(); // Returning current date as an example
+        const data = response.data;
+        const date = new Date().toString(); // Returning current date as an example
 
         // Limit the response if a limit parameter is provided
         const limitedData = limit ? data.slice(0, parseInt(limit)) : data;
 
-        return res.status(200).json({date,limitedData}); 
+        return res.status(200).json({ date, limitedData });
 
         // // Assuming the date is in the response, you can adjust to fit your data structure
 
