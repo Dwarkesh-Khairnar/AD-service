0d036e40ca5c76408138bcd67d1336097ad0cfac
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-01-03 22:21:08 +0530

    update ad overlay (work on onclick only)

--- src/Components/fileuploade.jsx
+++ src/Components/fileuploade.jsx
@@ -1,37 +1,54 @@
-import React, { useEffect } from "react";
+
+import React, { useEffect,useState } from "react";
 
 function fileuploade() {
+ const [showOverlay, setShowOverlay] = useState(false);
 
-  function test() {
-    console.log('test');
-    
-  }
   return (
-    <div className="h-full w-full bg-amber-600 flex absolute justify-center items-center">
-      <div className="md:w-150 md:h-100 w-full h-96">
-        <iframe
-          width={"100%"}
-          height={"100%"}
-          frameborder="0"
-          src="https://mega.nz/embed/Hcs2UQob#Qphj3vweCFAj1ATE_y2nrx3qsrIpFVnJ4erIxhbQtvo!1a1m"
-          // src="https://mega.nz/file/Hcs2UQob#Qphj3vweCFAj1ATE_y2nrx3qsrIpFVnJ4erIxhbQtvo"
-          allowfullscreen
-        ></iframe>
-      </div>
-
-      {/* <div className='p-6'>
-      <h1 className='text-2xl font-bold mb-4'>Ad Display</h1>
-      {videoLink ? (
-        <video controls className='rounded-lg border shadow-md'>
-        <source src={videoLink} type="video/mp4" />
-        </video>
-        ) : 'Loading...'}
-        </div> */}
-      <div className="md:w-150 md:h-100 w-full h-96 bg-transparent absolute">
-        <div className="md:w-150 md:h-10 w-full h-10 bg-black absolute flex justify-end pt-1 pr-1.5">
-          <span id="Stopad" className="z-20 cursor-pointer" onClick={test}>&#x274c;</span>
+    <div className="app">
+      <div>
+        <div
+          className="p-10 bg-violet-400 h-30 w-60 m-40"
+          onClick={() => setShowOverlay(true)}
+        >
+          Open Overlay
         </div>
       </div>
+
+      {/* GLOBAL OVERLAY */}
+      {showOverlay && (
+        <div
+          className=" fixed bg-black flex justify-center items-center z-100 inset-0"
+          onClick={() => setShowOverlay(true)}
+        >
+          <div
+            className="bg-white p-2 rounded-2xl text-center relative"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <span
+              className="absolute top-1.5 right-4 font-bold cursor-pointer hover:text-red-600"
+              onClick={() => setShowOverlay(false)}
+            >
+              &times;
+            </span>
+            <p className="mt-3">Ad run by dwarkesh and team</p>
+
+            <div className="md:w-150 md:h-100 w-full h-96">
+              <div className="w-full h-full bg-transparent absolute pr-4">
+                <div className="w-full h-10 bg-black"></div>
+              </div>
+              <iframe
+                width={"100%"}
+                height={"100%"}
+                frameborder="0"
+                src="https://mega.nz/embed/Hcs2UQob#Qphj3vweCFAj1ATE_y2nrx3qsrIpFVnJ4erIxhbQtvo!1a1m"
+                // src="https://mega.nz/file/Hcs2UQob#Qphj3vweCFAj1ATE_y2nrx3qsrIpFVnJ4erIxhbQtvo"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
