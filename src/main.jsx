b35adf3abfebd8c2de3a2f63737ee4f55951fead
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-01-23 21:29:25 +0530

    Ad run test 1 : Done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Ad_runing_component from "./Components/Ads components/Ad_runing_component.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <Ad_runing_component> */}
+      <App />
+    {/* </Ad_runing_component> */}
   </React.StrictMode>
 );
