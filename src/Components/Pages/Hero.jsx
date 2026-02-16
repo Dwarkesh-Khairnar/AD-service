f3cbb8e07b50ba01d2c519a63ab31de2b0676e6a
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-02-16 23:02:10 +0530

    Remove non maching section on home

--- src/Components/Pages/Hero.jsx
+++ src/Components/Pages/Hero.jsx
@@ -15,7 +15,7 @@ function Hero({ onSubmit }) {
 
   return (
     <>
-      <section className=" bg-teal-300 text-center mb-2 h-screen overflow-hidden justify-center">
+      <section className=" bg-teal-300 text-center mb-1 h-screen overflow-hidden justify-center">
         <div className=" bg-amber-400 h-72 w-[230%] mt-44 -ml-60 -rotate-45 md:h-[80%] md:-ml-[50%] md:mt-96 md:rotate-45">
           <h4 className=" absolute top-100 start-50 font-bold text-6xl text-white mask-linear-from-amber-700 w-2x1 mix-blend-color rotate-45  md:w-3x1 md:">
             WE ARE AD SARVICE PROVADER
@@ -31,30 +31,19 @@ function Hero({ onSubmit }) {
           advertising experience!.
         </p>
         <button className=" absolute start-[36%] top-112 bg-teal-400 rounded-md text-red-50 p-3 md:start-1/3 md:text-2xl md:top-110 ">
-          <Link rel="stylesheet" to="/plans">Get Start</Link> 
+          <Link rel="stylesheet" to="/plans">
+            Get Start
+          </Link>
         </button>
         <img
           src={Hero_svg}
           alt=""
           className="absolute end-[18%] w-60 md:top-90 md:end-17 md:w-100"
         />
-        ;
       </section>
 
       {/* What we do Section */}
-      <section>
-      <div className=" bg-amber-400 h-screen w-full overflow-y-hidden">
-        <div className=" z-10 bg-teal-300 h-72 w-[230%] top-269 start-0 md:h-[80%] md:-ml-[50%] md:mt-96 ">
-          <p className="font-bold text-3xl text-amber-500 ">What we do?</p>
-          <div className="flex w-full">
-            <div className="w-50">a</div>
-            <div className="w-50">a</div>
-            <div className="w-50">a</div>
-            <div className="w-50">a</div>
-        </div>
-          </div>
-        </div>
-      </section>
+     
     </>
   );
 }
