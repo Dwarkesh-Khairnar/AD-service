0e3b6bb9c7087143d782bcdb0894496152b1ebd7
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-02-22 23:46:14 +0530

    Cheange Header poistion moving to fix position

--- src/Components/Pages/Header.jsx
+++ src/Components/Pages/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import logo from "../../assets/img/logo_ad.png";
 
 function Header() {
-  const [visible, setVisible] = useState(false);
+  const [sidenav, setsidenav] = useState(false);
   const [svg, setSvg] = useState(1);
   const svgs = [
     <svg viewBox="4 0 500 450" fill="#DD0303 ">
-  <path xmlns="http://www.w3.org/2000/svg" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
     </svg>,
     <svg viewBox="4 0 500 450" fill="#fa812f">
       <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L96 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
@@ -15,60 +15,63 @@ function Header() {
   ];
 
   const toggleSidenav = () => {
-    setVisible(!visible);
+    setsidenav(!sidenav);
     // Update the index to show the next SVG
     setSvg((prevIndex) => (prevIndex + 1) % svgs.length);
   };
 
   return (
     <>
-      <header className=" bg-transparent p-2 pr-3 flex justify-between items-center -mb-16 ">
-        <img src={logo} alt="Logo" className="w-13"/>
+      <header className="bg-transparent fixed w-screen z-10">
+      <div className="w-screen mx-auto flex items-center text-blue-100 justify-between h-16 px-4">
+        <img src={logo} alt="Logo" className="w-13" />
         <div>
           <ul className="flex gap-3">
             <li className="z-50 hidden md:block hover:text-red-600"><Link to="/">Home</Link></li>
             <li className="z-50 hidden md:block hover:text-red-600"><Link to="/services">Services</Link></li>
             <li className="z-50 hidden md:block hover:text-red-600"><Link to="/plans">Plans</Link></li>
             {/* <li className="z-50 hidden md:block hover:text-red-600"><Link to="/placeholder">########</Link></li> */}
-            <li className="z-50 hidden md:block hover:text-red-600"><Link to="/schedule">Schedule Meeting</Link></li>
+            <li className="z-50 hidden md:block hover:text-red-600"><Link to="/schedule">Meeting</Link></li>
             <li className="z-50 hidden md:block hover:text-red-600"><Link to="/docs">Docs</Link></li>
-            
           </ul>
         </div>
         <div className="flex z-50">
           <button className="bg-yellow-500 hover:bg-amber-600 text-white py-2 px-3 rounded-bl-md">
             +New
           </button>
-          <div className="block md:hidden mr-0 m-2 w-6 cursor-pointer z-50" onClick={toggleSidenav} aria-expanded={visible} aria-controls="sidenav">
+          <div
+            className="block md:hidden mr-0 m-2 w-6 cursor-pointer z-50"
+            onClick={toggleSidenav}
+            aria-expanded={sidenav}
+            aria-controls="sidenav"
+          >
             {svgs[svg]}
           </div>
+        </div>
         </div>
       </header>
 
       <div
         id="sidenav"
         className={`top-0 transition-opacity duration-200 ease-in-out ${
-          visible ? "opacity-100 h-auto" : "opacity-0 h-0 pointer-events-none"
+          sidenav ? "opacity-100 h-auto" : "opacity-0 h-0 pointer-events-none"
         } overflow `}
       >
         <div className=" absolute top-0 end-0 w-3/4 h-full bg-amber-400 z-10 border-l-2 border-white">
           <h4 className="m-6 text-amber-400">Ad</h4>
           <hr className="text-blue-100 -mt-3" />
 
-          <ul className="mt-8 ml-20"> 
-              <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}> <Link to="/">Home</Link> </li> 
-              <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}> <Link to="/services">Services</Link> </li> 
-              <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}> <Link to="/plans">Plans</Link> </li> 
-              <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}> <Link to="/schedule">Schedule Meeting</Link> </li> 
-              <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}> <Link to="/#">Ragistration</Link> </li>
-              <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}> <Link to="/docs">Docs</Link> </li>
+          <ul className="mt-8 ml-20">
+            <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}>{" "}<Link to="/">Home</Link>{" "}</li>
+            <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}>{" "}<Link to="/services">Services</Link>{" "}</li>
+            <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}>{" "}<Link to="/plans">Plans</Link>{" "}</li>
+            <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}>{" "}<Link to="/schedule">Schedule Meeting</Link>{" "}</li>
+            <li className="my-4 hover:text-red-600"                 onClick={toggleSidenav}>{" "}<Link to="/#">Ragistration</Link>{" "}</li>
+            <li className="my-4 hover:text-red-600 text-amber-50" onClick={toggleSidenav}>{" "}<Link to="/docs">Docs</Link>{" "}</li>
           </ul>
           <hr className="text-blue-100" />
           <ul className="ml-20 mt-10">
-            <li className="text-amber-50 hover:text-red-600"onClick={toggleSidenav}>
-              {" "}
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
+            <li className="text-amber-50 hover:text-red-600"     onClick={toggleSidenav}>{" "}<Link to="/dashboard">Dashboard</Link></li>
           </ul>
         </div>
       </div>
