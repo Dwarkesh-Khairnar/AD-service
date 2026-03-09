aca6a9a3a2867081e7d51397e2d1ad33856811db
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-03-09 22:39:18 +0530

    Footer - new size and alingments

--- src/Components/Pages/Footer.jsx
+++ src/Components/Pages/Footer.jsx
@@ -9,7 +9,7 @@ setYear(new Date().getFullYear())
 
   return (
   <footer className="bg-[#071025] text-[#e9f6ff] pt-14 pb-8 px-10">
-    <div className="md:flex gap-20 max-w-[1100px] m-auto md:flex-row flex-col md:m-0 mt-5">
+    <div className="md:flex gap-20 max-w-275 m-auto md:flex-row flex-col md:m-0 mt-5">
         <div className="flex-1 min-w-[320px]">
             <h4 className='my-2 text-white font-extrabold'>About [Ad service]</h4>
             <p>ad service: scalable ad tech for publishers & advertisers. © <span>{year}</span> ad service</p>
@@ -18,8 +18,8 @@ setYear(new Date().getFullYear())
             <p className="address">J.T.Mahajan Collage Of Engg,Faizpur</p>
         </div>
 
-      <div className="md:flex gap-20 max-w-[1100px] m-auto md:flex-row flex-col md:m-0 mt-5">
-        <div className="flex-1 min-w-[220px] products">
+      <div className="md:flex gap-20 max-w-275 m-auto md:flex-row flex-col md:m-0 mt-5">
+        <div className="flex-1 min-w-55 products">
             <h4 className='my-2 text-white font-extrabold'>Products & Tools</h4>
             <ul>
                 <li className='ml-3 mt-2'><a className='text-[#7ad1ff]' href="/platform">Ad Platform</a></li>
@@ -29,7 +29,7 @@ setYear(new Date().getFullYear())
             </ul>
         </div>
 
-        <div className="flex-1 min-w-[220px] support md:m-0 mt-5">
+        <div className="flex-1 min-w-55 support md:m-0 mt-5">
             <h4 className='my-2 text-white font-extrabold'>Legal & Resources</h4>
             <ul className=''>
                 <li className='ml-3 mt-2'><a className='text-[#7ad1ff]' href="/privacy">Privacy Policy</a></li>
@@ -46,7 +46,7 @@ setYear(new Date().getFullYear())
       </div>
     </div>
     <div className=" border-t-2 border-[#ffffff0f] mt-12 pt-15">
-        <div className="flex gap-10 max-w-[1100px] m-auto justify-between align-middle">
+        <div className="flex gap-10 max-w-275 m-auto justify-between align-middle">
             <div className="font-bold text-[#bfe9ff]">Accepting: Visa, Mastercard, AMEX</div>
             <div className=" flex md:flex-row flex-col gap-1.5"><a href="/publisher-apply" className="bg-amber-500 text-[#012033] md:py-2 md:px-3 px-2 py-1 rounded-xl md:font-bold md:ml-2">Apply as Publisher</a> · <a 
                     href="/advertiser-apply" className="bg-amber-500 text-[#012033] md:py-2 md:px-3 px-2 py-1 rounded-xl md:font-bold md:ml-2 ">Advertiser Signup</a></div>
