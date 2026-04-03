00ee16a06d271b452caee6cfabf6a727048937b1
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-03 22:23:52 +0530

    SignupForm - New component for user registration with form handling and axios API integration

---
+++ src/Components/Auth/SingupFrom.jsx
@@ -0,0 +1,182 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function SingupFrom() {
+    const [FormData, setformData] = useState({
+      "Company_Name":"",
+      "mail":"",
+      "Secondry_mail":"",
+      "role":"",
+      "password":"",
+      "Confirm_password":"",
+      "key_words":"",
+    })
+
+    const SubmitHandel=(event)=>{
+        event.preventDefault();
+        console.log('FormData', FormData)
+        
+        axios.post("http://localhost:5000/testData",FormData)
+        .then((response)=>{
+           setResponseMessage("Post created successfully!");
+        })
+        .catch((err)=>{
+           setResponseMessage("Error creatingPost",err);
+        })
+    }
+
+    const valueAddHandelr = (event)=>{
+      const {name,value}=event.target; 
+
+     setformData((priData)=>({
+      ...priData,
+      [name]:value
+     }));
+
+    }
+
+  return (
+    <>
+    <div className='h-16'></div>
+     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center text-gray-800">
+         SingUp
+        </h2>
+        <form className="space-y-4" onSubmit={SubmitHandel}>
+          {/* Name Field */}
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Company Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your company name"
+              name='Company_Name'
+              onChange={valueAddHandelr}
+              />
+          </div>
+
+          {/* Email Field */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+              >
+             Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your email"
+              name='mail'
+              onChange={valueAddHandelr}
+              />
+          </div>
+          {/* Email Field */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+              >
+            Secondry Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your email"
+              name='Secondry_mail'
+              onChange={valueAddHandelr}
+              />
+          </div>
+          {/* Role Field */}
+          <div>
+            <label
+              htmlFor="Role"
+              className="block text-sm font-medium text-gray-700"
+              >
+              Role
+            </label>
+            <input
+              type="Role"
+              id="Role"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your Role"
+              name='role'
+              onChange={valueAddHandelr}
+              />
+          </div>
+          {/* Passwords Field */}
+          <div>
+            <label
+              htmlFor="time"
+              className="block text-sm font-medium text-gray-700"
+              >
+              Password
+            </label>
+            <input
+              type="password"
+              id="date"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter Password"
+              name='password'
+              onChange={valueAddHandelr}
+              />
+           
+          </div>
+          {/* Confirm passwords Field */}
+          <div>
+            <label
+              htmlFor="time"
+              className="block text-sm font-medium text-gray-700"
+              >
+             Confirm Password
+            </label>
+            <input
+              type="password"
+              id="date"
+              className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter Password"
+              name='Confirm_password'
+              onChange={valueAddHandelr}
+              />
+           
+          </div>
+
+        
+          <label htmlFor="Describe_Business">Describe Business</label>
+          <textarea
+            type="textarea"
+            name="Describe_Business"
+            id="Describe_Business"
+            className="w-full px-4 py-2 mt-1 text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+            maxlength="150" placeholder="useing key words in 150 char"
+            Name='key_words'
+            onChange={valueAddHandelr}
+            ></textarea>
+
+          {/* Submit Button */}
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-amber-500 rounded-md hover:bg-green-600 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              SingUp
+            </button>
+          </div>
+        </form>
+      
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default SingupFrom
