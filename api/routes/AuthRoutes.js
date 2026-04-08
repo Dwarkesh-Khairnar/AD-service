cc53f35b0e2fe83826961c410329601abc349a06
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-08 23:03:05 +0530

    AuthRoutes - Improve error handling during password hashing and fix variable reference

--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -8,10 +8,10 @@ router.post("/singup", async (req, res) => {
 
     try {
         bcrypt.genSalt(10, function (err, salt) {
-            if (err) return res.status(500).json({ message: "Error genreting salt" })
+            if (err) return res.status(500).json({ message: "Error genreting salt",Error:err})
 
-            bcrypt.hash(logInData.hast, salt, async function (err, hash) {
-                if (err) return res.status(500).json({ message: "Error hashing password" });
+            bcrypt.hash(data.password, salt, async function (err, hash) {
+                if (err) return res.status(500).json({ message: "Error hashing password",Error:err});
                 // DB 
                 const result = await client.query('insert into users(company_name,mail,secondary_mail,key_words,role,password) values($1,$2,$3,$4,$5,$6)', [data.company_name, data.mail, data.secondary_mail, data.key_words, data.role, hash]);
                 if (result.rowCount === 0) return res.status(400).json({ message: "User not create , Bad request" })
