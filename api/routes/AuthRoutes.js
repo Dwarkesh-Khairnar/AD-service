14b573683cdcc0075c4d3539abf580bbcbe22092
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-08 22:41:21 +0530

    AuthRoutes - Implement password hashing during user signup and update error handling

--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -7,12 +7,21 @@ router.post("/singup", async (req, res) => {
     let data = req.body;
 
     try {
-        const result = await client.query('insert into users(company_name,mail,secondary_mail,key_words,role,password) values($1,$2,$3,$4,$5,$6)', [data.company_name, data.mail, data.secondary_mail, data.key_words, data.role, data.password]);
-        if (result.rowCount === 0) return res.status(400).json({ message: "User not create , Bad request" })
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) return res.status(500).json({ message: "Error genreting salt" })
+
+            bcrypt.hash(logInData.hast, salt, async function (err, hash) {
+                if (err) return res.status(500).json({ message: "Error hashing password" });
+                // DB 
+                const result = await client.query('insert into users(company_name,mail,secondary_mail,key_words,role,password) values($1,$2,$3,$4,$5,$6)', [data.company_name, data.mail, data.secondary_mail, data.key_words, data.role, hash]);
+                if (result.rowCount === 0) return res.status(400).json({ message: "User not create , Bad request" })
+            });
+        });
+
         return res.status(201).json({ message: "User created successfully!" });
 
     } catch (error) {
-        return res.status(500).json({ message: "Server error: Insertion failed", Error: error })
+        return res.status(500).json({ message: "Database error: Insertion failed", Error: error })
 
     }
 })
