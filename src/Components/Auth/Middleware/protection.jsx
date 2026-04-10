0f2c84673d2b37e0d06fba210fcee53dfaea57f0
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-10 22:52:27 +0530

    Protection - create protection middleware for route access control sample-1 [not complite]

---
+++ src/Components/Auth/Middleware/protection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from 'react-router-dom'
+
+export default function protection({ children }) {
+   const token = "";
+
+   if (!token) return <Navigate to="/login" replace />;
+
+   return <Navigate to="/login" replace />;
+
+   return children;
+}
