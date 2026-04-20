135f540cfcb4f474dc8e412be47ca6e00b13fb13
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-20 23:14:02 +0530

    Protection - Retrieve token from localStorage for authentication check - temp for redis

--- src/Components/Auth/Middleware/protection.jsx
+++ src/Components/Auth/Middleware/protection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from 'react-router-dom'
 
 export default function protection({ children }) {
-   const token = "";
+   const token = localStorage.getItem("token")     //Temprery storage
 
    if (!token) return <Navigate to="/login" replace />;
 
