06d317e20b0ffda0ee94529fc655a38e7a877632
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-04-27 23:13:33 +0530

    apiRoutes - Comment api key verfication code and take temprary use of fetch-ad route

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -81,20 +81,20 @@ router.get('/fetch-ad', async (req, res) => {
     let { url, apiKey, limit } = req.query;
     let msg;
 
-    if (limit == 0 || limit == undefined) {
-        limit = 1;
-        msg = 'Set limit for fast and good parfect response but default limit is 1';
-    }
+    // if (limit == 0 || limit == undefined) {
+    //     limit = 1;
+    //     msg = 'Set limit for fast and good parfect response but default limit is 1';
+    // }
 
     // Validate URL
-    if (!url) {
-        return res.status(400).json({ error: 'Bad Request: URL is required' });
-    }
+    // if (!url) {
+    //     return res.status(400).json({ error: 'Bad Request: URL is required' });
+    // }
 
     try {
 
         // Await the database query to validate the API key
-        const result = await client.query(`SELECT * FROM public.api_keys WHERE Key = $1`, [apiKey]);
+    /*    const result = await client.query(`SELECT * FROM public.api_keys WHERE Key = $1`, [apiKey]);
 
         // Validate API Key - check if any rows were returned
         if (result.rows.length === 0) {
@@ -107,7 +107,7 @@ router.get('/fetch-ad', async (req, res) => {
         const data = response.data;
 
         const limitedData = limit ? data.slice(0, parseInt(limit)) : data;
-
+    */
         const link = await client.query(`select * from ad_links where isactive = true order by random() limit 1;`);
         if (link.rows.length === 0) {
             return res.status(401).json({ error: 'Your luck is good ad not aweleble right now' });
